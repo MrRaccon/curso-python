db7c2775d93ba7603b5f30448eb95eb1f83dbc3c
Alexis <luis.sosa@gnp.com.mx>
2021-09-14 17:12:11 -0500

    ciclos

---
+++ for.py
@@ -0,0 +1,19 @@
+def run():
+   contador = 1 
+   while contador < 1000:
+       contador +=1
+       print(contador)
+
+
+def runfor():
+    # a = list(range(1000))
+    # print(a)
+    for contador in range(1,1001):
+        print(contador)
+
+
+
+
+if __name__ == '__main__':
+    #run()
+    runfor()
